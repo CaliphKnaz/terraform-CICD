4ab5bed4e3d6ff5386b2d1ed5203ee0263bf6e39
Tomi <48963553+CaliphKnaz@users.noreply.github.com>
2023-01-02 21:14:03 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 terraform {
   required_version = ">=0.12.13"
   #backend "s3" {
-  #  bucket         = "kyler-github-actions-demo-terraform-tfstate"
+  #  bucket         = "test-demo-terraform-tfstate"
   #  key            = "terraform.tfstate"
   #  region         = "us-east-1"
   #  dynamodb_table = "aws-locks"
@@ -22,7 +22,7 @@ provider "aws" {
 # Call the seed_module to build our ADO seed info
 module "bootstrap" {
   source                      = "./modules/bootstrap"
-  name_of_s3_bucket           = "kyler-github-actions-demo-terraform-tfstate"
+  name_of_s3_bucket           = "test-demo-terraform-tfstate"
   dynamo_db_table_name        = "aws-locks"
   iam_user_name               = "GitHubActionsIamUser"
   ado_iam_role_name           = "GitHubActionsIamRole"
