5573c5ce104744e033018569f1540f392166824c
Tomi <48963553+CaliphKnaz@users.noreply.github.com>
2023-01-03 22:33:00 +0000

    terraform test

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 # Require TF version to be same as or greater than 0.12.13
 terraform {
   required_version = ">=0.12.13"
-  #backend "s3" {
-  #  bucket         = "test-demo-terraform-tfstate"
-  #  key            = "terraform.tfstate"
-  #  region         = "us-east-1"
-  #  dynamodb_table = "aws-locks"
-  #  encrypt        = true
-  #}
+  backend "s3" {
+    bucket         = "test-demo-terraform-tfstate"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "aws-locks"
+    encrypt        = true
+  }
 }
 
 # Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
@@ -17,9 +17,7 @@ provider "aws" {
 }
 
 
-/* Commented out until after bootstrap
 
-# Call the seed_module to build our ADO seed info
 module "bootstrap" {
   source                      = "./modules/bootstrap"
   name_of_s3_bucket           = "test-demo-terraform-tfstate"
@@ -29,6 +27,7 @@ module "bootstrap" {
   aws_iam_policy_permits_name = "GitHubActionsIamPolicyPermits"
   aws_iam_policy_assume_name  = "GitHubActionsIamPolicyAssume"
 }
+
 
 # Build the VPC
 resource "aws_vpc" "vpc" {
